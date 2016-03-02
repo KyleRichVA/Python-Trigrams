de9ae8e632bfc7a589a09fe3f476593221f11b6c
Kyle R <KyleRichVA@gmail.com>
2016-03-02 14:31:26 -0800

    sucessfull tests on converting text to list of words only with (a-z,A-Z,0-9)

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -3,16 +3,15 @@
 import pytest
 
 
-@pytest.mark.parametrize('inputFile', './sherlock_small.txt')
-def test_get_file_txt(inputFile):
+def test_get_file_txt():
     from trigrams import get_file_txt
-    assert len(get_file_txt(inputFile)) > 0
+    assert len(get_file_txt('./sherlock_small.txt')) > 0
 
 testStr = "hello, my name Is Kyle Richardson! 45 %%%%"
 expectedList = ['hello', 'my', 'name', 'Is', 'Kyle', 'Richardson', '45']
 
 
-@pytest.mark.parametrize('testStr, expectedList', [testStr, expectedList])
+@pytest.mark.parametrize('text, expected', [(testStr, expectedList)])
 def test_txt_to_tri(text, expected):
     from trigrams import txt_to_tri
     assert txt_to_tri(text) == expected
--- src/trigrams.py
+++ src/trigrams.py
@@ -2,6 +2,7 @@
 
 import io
 import random
+import re
 
 
 def get_file_txt(inputFile):
@@ -10,3 +11,12 @@ def get_file_txt(inputFile):
     f.close()
     return file_txt
 
+
+def txt_to_tri(text):
+    str_list = text.split(" ")
+    print str_list
+    for ind, word in enumerate(str_list):
+        str_list[ind] = re.sub("[^\w]", "", word)
+        str_list = [x for x in str_list if x]
+    print str_list
+    return str_list
