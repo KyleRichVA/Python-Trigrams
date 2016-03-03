ba2644896e326b6e7487681b73e09b9321815581
Kyle R <KyleRichVA@gmail.com>
2016-03-03 10:57:06 -0800

    fixed up everything to PEP standards

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,24 +1,28 @@
-# -*- coding; utf-8 -*-
-# created by Kyle Richardson and AJ Wohlfert
-# Trigrams.py generates a random piece of text based of user entered text file.
+"""Trigrams.py.
 
+Generates a random piece of text based of user entered text file
+-*- coding; utf-8 -*-
+Created by Kyle Richardson and AJ Wohlfert
+"""
 import io
 import random
 import re
 from sys import argv
 
 
-def get_file_txt(inputFile):
-    '''Take a text file(inputFile) and returns it's text'''
-    f = io.open(inputFile)
+def get_file_txt(inputfile):
+    """Take a text file(inputfile) and returns it's text."""
+    f = io.open(inputfile)
     file_txt = f.read()
     f.close()
     return file_txt
 
 
 def txt_to_list(text):
-    '''Take a single string(text), and returns a list of its words ingoring or
-    replacing non alphabetical or numeric characters'''
+    """Take a single string(text), and returns a list of its words.
+
+    ingores or replaces non alphabetical or numeric characters.
+    """
     # replaces newlines and '-' with a single space
     spaced_text = re.sub(r"[\r\n]|[+-]", ' ', text)
     str_list = spaced_text.split(" ")
@@ -30,46 +34,48 @@ def txt_to_list(text):
     return str_list
 
 
-def list_to_tri(InputList):
-    '''Return a trigram dict based off InputList'''
+def list_to_tri(inputlist):
+    """Return a trigram dict based off inputlist."""
     trigram = {}
-    while len(InputList) >= 3:
-        key1 = InputList[-3]
-        key2 = InputList[-2]
-        value = InputList.pop(-1)
+    while len(inputlist) >= 3:
+        key1 = inputlist[-3]
+        key2 = inputlist[-2]
+        value = inputlist.pop(-1)
         # Attempt to append value to key if it is in the trigram dict.
         trigram.setdefault((key1, key2), []).append(value)
     return trigram
 
 
-def get_rand_triKey(trigram):
-    '''Return a random key from a trigram dict'''
+def get_rand_trikey(trigram):
+    """Return a random key from a trigram dict."""
     return random.choice(list(trigram.keys()))
 
 
-def generate_text(trigram, numWords):
-    '''Generate (numWords) ammount of words in a single string
-    based off the (trigram) dict'''
+def generate_text(trigram, num_words):
+    """Generate (num_words) ammount of words in a single string.
+
+    Based off the (trigram) dict.
+    """
     # set the first two words as a random key in trigram
-    wordList = list(get_rand_triKey(trigram))
-    while len(wordList) < numWords:
+    word_list = list(get_rand_trikey(trigram))
+    while len(word_list) < num_words:
         # if key exists add one trigram value to generated text
-        if (wordList[-2], wordList[-1]) in trigram.keys():
-            potentialWords = trigram[(wordList[-2], wordList[-1])]
-            wordList.append(potentialWords[
-                random.randint(0, len(potentialWords)-1)])
+        if (word_list[-2], word_list[-1]) in trigram.keys():
+            potential_words = trigram[(word_list[-2], word_list[-1])]
+            word_list.append(potential_words[
+                random.randint(0, len(potential_words) - 1)])
         # if a trigram cannot be found select a random key and add the words
         else:
-            randKey = get_rand_triKey(trigram)
-            wordList.append(randKey[0])
-            wordList.append(randKey[1])
+            rand_key = get_rand_trikey(trigram)
+            word_list.append(rand_key[0])
+            word_list.append(rand_key[1])
     # clear out any extra words that may have been made
-    wordList = wordList[:numWords]
-    return ' '.join(wordList)
+    word_list = word_list[:num_words]
+    return ' '.join(word_list)
 
 if __name__ == "__main__":
-    script, fileName, numWords = argv
+    script, fileName, num_words = argv
     fileTxt = get_file_txt(fileName)
     strList = txt_to_list(fileTxt)
     trigram = list_to_tri(strList)
-    print(generate_text(trigram, int(numWords)))
+    print(generate_text(trigram, int(num_words)))
