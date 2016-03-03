d3ed0c713f46a764d37e25183ed7e3efa418a77f
Kyle R <KyleRichVA@gmail.com>
2016-03-03 10:20:47 -0800

    change the trigram setup to a setdefault() function, looks nicer

--- src/trigrams.py
+++ src/trigrams.py
@@ -9,7 +9,7 @@ from sys import argv
 
 
 def get_file_txt(inputFile):
-    '''Takes a text file(inputFile) and returns it's text'''
+    '''Take a text file(inputFile) and returns it's text'''
     f = io.open(inputFile)
     file_txt = f.read()
     f.close()
@@ -17,7 +17,7 @@ def get_file_txt(inputFile):
 
 
 def txt_to_list(text):
-    '''Takes a single string(text), and returns a list of its words ingoring or
+    '''Take a single string(text), and returns a list of its words ingoring or
     replacing non alphabetical or numeric characters'''
     # replaces newlines and '-' with a single space
     spaced_text = re.sub(r"[\r\n]|[+-]", ' ', text)
@@ -31,28 +31,24 @@ def txt_to_list(text):
 
 
 def list_to_tri(InputList):
-    '''return a trigram dict based off InputList'''
+    '''Return a trigram dict based off InputList'''
     trigram = {}
     while len(InputList) >= 3:
         key1 = InputList[-3]
         key2 = InputList[-2]
         value = InputList.pop(-1)
         # Attempt to append value to key if it is in the trigram dict.
-        try:
-            trigram[(key1, key2)].append(value)
-        # If a Key Error is thrown, make a new list for that key
-        except KeyError:
-            trigram[(key1, key2)] = [value]
+        trigram.setdefault((key1, key2), []).append(value)
     return trigram
 
 
 def get_rand_triKey(trigram):
-    '''return a random key from a trigram dict'''
+    '''Return a random key from a trigram dict'''
     return random.choice(list(trigram.keys()))
 
 
 def generate_text(trigram, numWords):
-    '''generate (numWords) ammount of words in a single string
+    '''Generate (numWords) ammount of words in a single string
     based off the (trigram) dict'''
     # set the first two words as a random key in trigram
     wordList = list(get_rand_triKey(trigram))
